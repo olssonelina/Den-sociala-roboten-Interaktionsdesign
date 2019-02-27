d1b76c5976d62f2102f4b54a60ca585dbd91f52a
Elina Olsson <elinao@student.chalmers.se>
2019-02-27 15:00:42 +0100

    Start of tutorial

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-from guizero import App
+from guizero import App, Text
 
 app = App(title="Hello world")
+
+welcome_message = Text(app, text="Welcome to my app", size=40, font="times new roman", color="red")
+
 app.display()
 
+# https://projects.raspberrypi.org/en/projects/getting-started-with-guis/7
